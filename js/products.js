eaf7456866d8e9df223c948b75973aef18829f22
Melina Mendez <mndzmelina@gmail.com>
2020-08-12 16:43:17 -0300

    Login

--- js/products.js
+++ js/products.js
@@ -1,3 +1,47 @@
+var productsArray = [];
+
+function showProductsList(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name + `</h4>
+                        <small class="text-muted">` + product.soldCount + ` artículos</small>
+                    </div>
+                ${product.description}
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            //Muestro los productos ordenados
+            showProductsList(productsArray);
+        }
+    });
+});
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
